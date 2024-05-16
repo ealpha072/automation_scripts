bb82e57cf0c35f95e157d67323004ffa5321e92c
ealpha072 <ealpha072@gmail.com>
2024-05-16 22:48:17 +0300

    Add: Changes to auto push

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ echo "All:Default (*) [1]"
 echo "Current (.) [2]"
 
 #add option for file
-read -p "Pick option [1,2]: " stage_option
+read -p "Pick option [1,2]: "stage_option
 
 if [ $stage_option -eq 1 ];then
     echo "Adding all changes"
@@ -17,11 +17,11 @@ else
 fi
 
 read -p "Commit header [fix, add, update...]: "commit_head
-read -p "Commit message: " commit_msg
-git commit -m "$commi_head: $commit_msg"
+read -p "Commit message: "commit_msg
+git commit -m "$commit_head: $commit_msg"
 
 read -p "Push changes? [y/n]: "push_ch
 
-if [ $push_ch == "y" ];then
+if [ "$push_ch" == "y" ];then
     git push -v
 fi
