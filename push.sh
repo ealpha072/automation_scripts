433d1d016a1153052d3eb7fb029c388a20b15a30
ealpha072 <ealpha072@gmail.com>
2024-05-17 21:34:06 +0300

    Fix: Solve variable name echoing

--- push.sh
+++ push.sh
@@ -1,27 +1,34 @@
-echo "Select changes to add"
-echo "All:Default (*) [1]"
-echo "Current (.) [2]"
+#check git status
 
-#add option for file
-read -p "Pick option [1,2]: "stage_option
+if [[ -n $(git status -s) ]]; then
 
-if [ $stage_option -eq 1 ];then
-    echo "Adding all changes"
-    git add *
-elif [ $stage_option -eq 2 ]; then
-    echo "Adding current changes"
-    git add .
+    echo "Select changes to add"
+    echo "All:Default (*) [1]"
+    echo "Current (.) [2]"
+
+    #add option for file
+    read -p "Pick option [1,2]: " stage_option
+
+    if [ $stage_option -eq 1 ];then
+        echo "Adding all changes"
+        git add *
+    elif [ $stage_option -eq 2 ]; then
+        echo "Adding current changes"
+        git add .
+    else
+        echo "Adding all changes"
+        git add *
+    fi
+    
+    #commit message and header
+    read -p "Commit message: " commit_msg
+    git commit -m "$commit_msg"
+
+    read -p "Push changes? [y/n]: " push_ch
+
+    if [ "$push_ch" == "y" ];then
+        git push -v
+    fi
 else
-    echo "Adding all changes"
-    git add *
-fi
-
-read -p "Commit header [fix, add, update...]: "commit_head
-read -p "Commit message: "commit_msg
-git commit -m "$commit_head: $commit_msg"
-
-read -p "Push changes? [y/n]: "push_ch
-
-if [ "$push_ch" == "y" ];then
-    git push -v
+    echo "No changes to commit"
 fi
